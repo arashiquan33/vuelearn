86da050327217c1da30861dc69a4c7366ccdf6af
tc.quan <304033826@qq.com>
2020-03-05 17:10:36 +0800

    Initial commit

---
+++ components/MyInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{value}}</p>
+    <input type="text" :value="value" v-on:input="write">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: "默认数据"
+    }
+  },
+  methods: {
+    write($event) {
+      this.$emit("input", $event.target.value);
+    }
+  }
+};
+</script>
+
